41bfee4735b7e31a030f9f308cf124bc48657eab
egoneau <infos@hello-interactiv.com>
2016-05-12 23:22:41 +0200

    Delete Unirest.php

--- test/libs/Unirest/Unirest.php
+++
@@ -1,7 +0,0 @@
-<? 
-
-require_once dirname(__FILE__) . '/Unirest/File.php';
-require_once dirname(__FILE__) . '/Unirest/Method.php';
-require_once dirname(__FILE__) . '/Unirest/Response.php';
-require_once dirname(__FILE__) . '/Unirest/Request.php';
-?>
